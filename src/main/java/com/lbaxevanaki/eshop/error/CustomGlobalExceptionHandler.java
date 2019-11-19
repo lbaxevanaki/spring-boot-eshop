bc8e9f63d8b5203cb2a044fa8c41d3a3e0fd313f
lbaxevanaki <leto.baxevanaki@seecago.com>
2019-11-19 13:56:59 +0200

    Refined Exception Handling

--- src/main/java/com/lbaxevanaki/eshop/error/CustomGlobalExceptionHandler.java
+++ src/main/java/com/lbaxevanaki/eshop/error/CustomGlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.lbaxevanaki.eshop.error;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +23,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	 private static final Logger log = LoggerFactory.getLogger(CustomGlobalExceptionHandler.class);
 
     // error handle for @Valid
     @Override
@@ -41,7 +47,23 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(body, headers, status);
 
     }
-  
-    
 
+ 
+    @ExceptionHandler({ EntityNotFoundException.class })
+    protected ResponseEntity<Object> handleNotFound(final RuntimeException ex, final WebRequest request) {
+       
+        final ApiError apiError =  new ApiError(
+                HttpStatus.NOT_FOUND, ex.getMessage(), "Entity with id:"+ ex.getMessage() + " does not exist.");
+        		
+        return handleExceptionInternal(ex, apiError, new HttpHeaders(), apiError.getStatus(), request);
+    }
+   
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+        ApiError apiError = new ApiError(
+          HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "error occurred");
+        return new ResponseEntity<Object>(
+          apiError, new HttpHeaders(), apiError.getStatus());
+    }
+    
 }
